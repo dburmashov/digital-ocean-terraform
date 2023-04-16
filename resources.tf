09c25e71f2684c869ecbc6ccaa60d355d3ca7562
Dmitry Burmashov <dmitry.burmashov@gmail.com>
2023-04-16 16:17:39 +0300

    initial coommit

---
+++ resources.tf
@@ -0,0 +1,26 @@
+resource "digitalocean_tag" "devops" {
+  name = "devops"
+}
+
+resource "digitalocean_tag" "user_email" {
+  name = "user_email"
+}
+
+
+resource "digitalocean_ssh_key" "rebrain_ssh_key" {
+  name       = "rebrain_ssh_key"
+  public_key = file(var.rebrain_ssh_key_path)
+}
+
+resource "digitalocean_ssh_key" "user_ssh_key" {
+  name       = "user_ssh_key"
+  public_key = file(var.user_ssh_key_path)
+}
+
+
+resource "digitalocean_droplet" "lab" {
+  image  = var.droplet_settings.image
+  name   = var.droplet_settings.name
+  size   = var.droplet_settings.size
+  tags   = [digitalocean_tag.devops.id, digitalocean_tag.user_email.id]
+}
---
+++ variables.tf
@@ -0,0 +1,25 @@
+variable "digital_ocean_token" {
+   description = "token for using api"
+}
+
+variable "rebrain_ssh_key_path" {
+   description = "rebrain_ssh_key_path"
+}
+
+variable "user_ssh_key_path" {
+   description = "user_ssh_key_path"
+}
+
+variable "user_email" {
+   description = "user email in the format ii_at_rebrainme_com for ii@rebrainme.com"
+}
+
+variable "droplet_settings" {
+   type = map(string)
+   default = {
+      image = "ubuntu-18-04-x64"
+      name = "vm1"
+      size = "s-1vcpu-1gb"
+  }
+}
+
